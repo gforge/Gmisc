5af28de7037b58f4951cd6723a552f8d2a52810b
Max Gordon <max@gforge.se>
2014-02-16 13:14:12 +0100

    Started with some visual testing

---
+++ visual_tests/bezierArrowSmpl.R
@@ -0,0 +1,10 @@
+# Ugly start - need to fix
+tmp <- list(x=c(.1,.3,.2,.9),
+            y=c(0.2, 0.9, 0.1, 0.9))
+
+grid.newpage()
+ag <- bezierArrowSmpl(x = tmp$x, y = tmp$y)
+grid.draw(ag)
+
+grid.bezier(x = tmp$x,
+            y = tmp$y, gp=gpar(col="red"))
