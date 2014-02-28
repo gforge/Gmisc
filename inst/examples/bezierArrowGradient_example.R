4d8c6b67bfc523375ee1a20d33f998167dd450b9
Max Gordon <max@gforge.se>
2014-02-28 12:39:52 +0100

    Minor fixes

--- inst/examples/bezierArrowGradient_example.R
+++ inst/examples/bezierArrowGradient_example.R
@@ -1,32 +1,32 @@
 par_org <- par(ask=TRUE) 
 library(grid)
 gl <- bezierArrowGradient(x=c(.2, .5, .5, .8),
-                              y=c(.2, .2, .8, .8),
-                              width=unit(10, "mm"),
-                              arrow = list(base=unit(.1, "npc"),
-                                length = unit(.1, "npc")),
-                              grdt_type = "triangle",
-                              grdt_decrease_prop = .3,
-                              grdt_start_prop = .5,
-                              grdt_clr_prop = .7,
-                              grdt_line_width = unit(6, "pt"),
-                              grdt_clr = "#2F4F2F")
-                            
+                          y=c(.2, .2, .8, .8),
+                          width=unit(10, "mm"),
+                          arrow = list(base=unit(.1, "npc"),
+                                       length = unit(.1, "npc")),
+                          grdt_type = "triangle",
+                          grdt_decrease_prop = .3,
+                          grdt_start_prop = .5,
+                          grdt_clr_prop = .7,
+                          grdt_line_width = unit(6, "pt"),
+                          grdt_clr = "#2F4F2F")
 
-  
-                    
+
+
+
 plot.new()
 grid.draw(gl)
 
 pg <- bezierArrowGradient(x=c(.3,.7,-.1,.25),
-  y=c(.5,.1,.1,.5),
-  grdt_type = "triangle",
-  grdt_decrease_prop = .3,
-  grdt_start_prop = .5,
-  grdt_clr_prop = .7,
-  grdt_line_width = unit(6, "pt"),
-  grdt_clr = "#2F4F2F",
-  align_2_axis=FALSE)
+                          y=c(.5,.1,.1,.5),
+                          grdt_type = "triangle",
+                          grdt_decrease_prop = .3,
+                          grdt_start_prop = .5,
+                          grdt_clr_prop = .7,
+                          grdt_line_width = unit(6, "pt"),
+                          grdt_clr = "#2F4F2F",
+                          align_2_axis=FALSE)
 plot.new()
 grid.draw(pg)
 
