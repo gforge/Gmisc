7e50b4eab4d6c0622bd3226efacc3d8792c8145e
Max Gordon <max@gforge.se>
2014-12-11 14:31:22 +0100

    Change to test_check

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,9 +1,3 @@
 library('testthat')
-library('Gmisc')
 
-test_package('Gmisc')
-## To run the tests manually
-if (FALSE){
-  my_test_dir <- gsub("Gmisc.*", "Gmisc/tests/testthat", getwd())
-  test_dir(my_test_dir)
-}
+test_check('Gmisc')
