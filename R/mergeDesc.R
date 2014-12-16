2fb8bbb1545eea695b986133ec372497ad445945
Max Gordon <max@gforge.se>
2014-12-16 13:34:08 +0100

    Added a latex wrapper for descMRg objects

--- R/mergeDesc.R
+++ R/mergeDesc.R
@@ -99,3 +99,29 @@ mergeDesc <- function(tlist, ...){
                    n.rgroup = n.rgroup,
                    class = c("descMrg", class(mx))))
 }
+
+
+#' A wrapper around the \code{\link[Hmisc]{latex}} function
+#' 
+#' Calls the \code{\link[Hmisc]{latex}} after extracting the
+#' \code{rgroup}/\code{n.rgroup} arguments.
+#' 
+#' @param x The \code{\link{mergeDesc}} object
+#' @param ... Passed onto \code{\link[Hmisc]{latex}}
+#' 
+#' @importFrom Hmisc latex
+#' @keywords internal
+#' @export
+latex.descMrg <- function(x, ...)
+{
+  dots <- list(...)
+  if (!"rgroup" %in% names(dots)){
+    return(NextMethod(generic = NULL, object = x,
+                      rgroup = attr(x, "rgroup"),
+                      n.rgroup = attr(x, "n.rgroup"),
+                      ...))
+  }
+  
+  return(NextMethod(generic = NULL, object = x,
+                    ...))
+}
