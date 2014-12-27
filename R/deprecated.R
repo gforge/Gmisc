3a010fee55ef2a87d1536cf58bd49e9a5e671ecf
Max Gordon <max@gforge.se>
2014-12-27 21:38:45 +0100

    Moved forestplot2 into a separate package

---
+++ R/deprecated.R
@@ -0,0 +1,18 @@
+#' Deprecated function(s) in \pkg{Gmisc}
+#'
+#' The \code{forestplot2} has move to \pkg{forestplot} package and changed name to \code{\link[forestplot]{forestplot}}
+#'
+#' @param ... Parameters to be passed to the new version of the function
+#' @export
+#' @keywords internal
+#' @import forestplot
+#' @rdname Gmisc-deprecated
+#' @name Gmisc-deprecated
+#' @docType package
+forestplot2 <- function(...){
+  .Deprecated(new = "forestplot",
+              package = "forestplot",
+              old = "forestplot2",
+              msg = "The forestplot2 has moved to a separate package as of 1.0, the 'forestplot' package")
+  forestplot(...)
+}
