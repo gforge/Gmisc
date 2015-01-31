7ba98a4a6c38f042fd2411914b3433b9eddaeaae
Max Gordon <max@gforge.se>
2015-01-31 16:26:40 +0100

    Changed clr for tests

--- tests/visual_tests/bezierArrowSmpl_vtests.R
+++ tests/visual_tests/bezierArrowSmpl_vtests.R
@@ -13,47 +13,50 @@ bezierArrowSmpl(x = tmp$x, y = tmp$y) %>%
 bezierArrowGradient(x = tmp$x+.05, y = tmp$y-.15) %>%
   grid.draw
 
+bezierArrowGradient(x = tmp$x-.05, y = tmp$y+.15, align_2_axis = TRUE) %>%
+  grid.draw
+
 drawBzrBasics <- function (bzrFn, ...) {
   grid.newpage()
   tmp <- list(x=c(.1, .1, .9, .9),
               y=c(.8, .8, .8, .8))
   ag <- bzrFn(x = tmp$x, y = tmp$y, ...) %>%
     grid.draw
-  
+
   tmp$x <- rev(tmp$x)
   tmp$y <- tmp$y - .1
   ag <- bzrFn(x = tmp$x, y = tmp$y, ...) %>%
     grid.draw
-  
-  
+
+
   tmp <- list(x=c(.7, .7, .7, .7),
               y=c(.6, .6, .1, .1))
-  
+
   ag <- bzrFn(x = tmp$x, y = tmp$y, ...) %>%
     grid.draw
-  
-  
+
+
   tmp <- list(x=c(.3, .3, .3, .3),
               y=c(.1, .1, .6, .6))
-  
+
   ag <- bzrFn(x = tmp$x, y = tmp$y, ...) %>%
     grid.draw
-  
+
   tmp <- list(x=c(.4, .4, .5, .5),
               y=c(.4, .4, .5, .5))
   ag <- bzrFn(x = tmp$x, y = tmp$y, ...) %>%
     grid.draw
-  
+
   tmp <- list(x=c(.4, .4, .5, .5) + .15,
               y=c(.5, .5, .4, .4))
   ag <- bzrFn(x = tmp$x, y = tmp$y, ...) %>%
     grid.draw
-  
+
   tmp <- list(x=c(.5, .5, .4, .4),
               y=c(.4, .4, .5, .5) - .2)
   ag <- bzrFn(x = tmp$x, y = tmp$y, ...) %>%
     grid.draw
-  
+
   tmp <- list(x=c(.5, .5, .4, .4) + .15,
               y=c(.5, .5, .4, .4) - .2)
   ag <- bzrFn(x = tmp$x, y = tmp$y, ...) %>%
@@ -61,5 +64,5 @@ drawBzrBasics <- function (bzrFn, ...) {
 }
 
 drawBzrBasics(bezierArrowSmpl, clr = "darkred")
-drawBzrBasics(bezierArrowSmpl, clr = "darkred", align_2_axis = TRUE)
+drawBzrBasics(bezierArrowSmpl, clr = "purple", align_2_axis = TRUE)
 drawBzrBasics(bezierArrowGradient, clr = "darkblue", grdt_clr = "yellow", grdt_clr_prop = .1, grdt_decrease_prop = .1)
