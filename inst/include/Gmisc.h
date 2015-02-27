a99ee12dec3e7ce23750043c7c074a0013b029e0
Max Gordon <max@gforge.se>
2015-02-27 23:42:53 +0100

    Added a c++ Lines class

---
+++ inst/include/Gmisc.h
@@ -0,0 +1,32 @@
+# include <vector>
+# include <iostream>
+# include <iomanip>
+# include <vector>
+
+struct Point{
+  double x;
+  double y;
+  bool problematic;
+};
+
+class Line {
+public:
+  Line(int length);
+	void addPoint(Point p);
+	void addPoint(double x, double y, bool problematic);
+	int removeIntersections();
+	int size();
+	std::vector<double> getX();
+	std::vector<double> getY();
+	std::vector<bool> getProblematic();
+private:
+	std::vector<Point> m_Points;
+	int _removeIntersect(std::vector<Point>::size_type err_point);
+	bool _getLineIntersection(Point p0,
+		Point p1,
+		Point p2,
+		Point p3,
+		Point *i_p);
+	void _smoothLine(int start, int end, Point int_point);
+	unsigned _nChooseK(unsigned n, unsigned k);
+};
