4c6d080f42b021618d87835179d32e82419bf8cd
Max Gordon <max@gforge.se>
2015-02-27 23:38:14 +0100

    The bezierArrowGradient has a simpler handling of the gradient proportion

--- R/bezierArrowGradient.R
+++ R/bezierArrowGradient.R
@@ -12,14 +12,18 @@
 #' @param grdt_type The type of growth and gradient that is to be used,
 #'  currently it only supports triangle (I'm considering adding bezier curves
 #'  but currently I'm a little tired of coding)
-#' @param grdt_decrease_prop The proportion of the full length that should be decreasing.
-#' @param grdt_start_prop The proportion of the full length that should be a constant color
-#'  before decreasing.
-#' @param grdt_clr_prop The proportion of the gradient that should be decreasing. This is a
-#'  proportion of the grdt_decrease_prop and the grdt_start_prop combined.
+#' @param grdt_prop The proportion of the full length that should be a the gradient. The gradient
+#'  consists of three things:
+#'  (1) the central band,
+#'  (2) the slimming of the central band,
+#'  (3) the color shift into the arrow color.
+#'  \emph{Note} that the the slimmin and color proportions can be overlapping.
+#' @param grdt_decrease_prop The proportion of the gradient that should be decreasing,
+#'  i.e. narrowing according to the \code{grdt_type} argument.
+#' @param grdt_clr_prop The proportion of the gradient that should be converging to the arrow color.
 #' @param grdt_line_width The width of the border line. If not specified it defaults to 5 \% of
 #'  the original width, note the gradient's width is thus 90 \%.
-#' @param grdt_clr The color of the gradient. It is the color that transits into the clr of the arrow.
+#' @param grdt_clr The color of the gradient.
 #' @param ... Passed on to \code{\link{bezierArrowSmpl}}
 #' @return \code{grid::gList()} A grob of \code{\link[grid]{gList}}-type
 #'
@@ -30,66 +34,73 @@
 #'                                      y = c(0.2, 0.2, 0.9, 0.9))
 #' grid.draw(arrowGrob)
 #' @export
-bezierArrowGradient <- function(
-  width = .05,
-  clr = "#000000",
-  default.units = "npc",
-  align_2_axis = TRUE,
-  grdt_type = c("triangle", "rectangle"),
-  grdt_decrease_prop = .4,
-  grdt_start_prop = .4,
-  grdt_clr_prop = .7,
-  grdt_line_width = NA,
-  grdt_clr = "#2F4F2F",
-  vp = NULL,
-  gp = gpar(),
-  ...){
+bezierArrowGradient <- function(x = c(0.2, .7, .3, .9),
+                                y = c(0.2, .2, .9, .9),
+                                width = .05,
+                                clr = "#000000",
+                                default.units = "npc",
+                                align_2_axis = TRUE,
+                                grdt_type = c("triangle", "rectangle"),
+                                grdt_prop = .8,
+                                grdt_decrease_prop = .5,
+                                grdt_clr_prop = .7,
+                                grdt_line_width,
+                                grdt_clr = "#2F4F2F",
+                                vp = NULL,
+                                gp = gpar(),
+                                ...){
+
+  if ("grdt_start_prop" %in% names(list(...)))
+    stop("The grdt_start_prop has been deprecated. Switch to grdt_prop instead.",
+         "\n**Note** The grdt_prop is a total of how large proportion of the arrow",
+         " should be covered with a gradient. This is different from before where",
+         " the grdt_start_prop was added to the grdt_decrease_prop in order to",
+         " generate this value.")
 
   # Get initial values
   grdt_type <- match.arg(grdt_type)
 
-  if (class(width) != "unit")
-    width <- unit(width, default.units)
+  # Adapt line width to the direction if default.units == "npc"
+  width <- getAbsoluteWidth(w = width,
+                            default.units = default.units,
+                            x = x,
+                            y = y)
 
-  if (is.na(grdt_line_width)){
-    grdt_line_width <- getGridVal(width, default.units)*.2
+  if (missing(grdt_line_width)){
+    grdt_line_width <- unit(getGridVal(width, "mm")*.5, units = "mm")
   }
+
+  grdt_line_width <- getAbsoluteWidth(w = grdt_line_width,
+                                      default.units = default.units,
+                                      x = x,
+                                      y = y)
 
   if (!inherits(grdt_line_width, "unit")){
     grdt_line_width <- unit(grdt_line_width, default.units)
   }
 
   # Sanity check for input parameters
+  if (grdt_prop > 1 &&
+        grdt_prop < 0)
+    stop("The gradient proportion (grdt_prop) must be between 0-1! You provided ", grdt_prop)
+
   if (grdt_decrease_prop > 1 &&
     grdt_decrease_prop < 0)
     stop("The decrease proportion must be between 0-1! You provided ", grdt_decrease_prop)
-
-  if (grdt_start_prop > 1 &&
-    grdt_start_prop < 0)
-    stop("The start proportion must be between 0-1! You provided ", grdt_start_prop)
 
   if (grdt_clr_prop > 1 &&
     grdt_clr_prop < 0)
     stop("The color gradient proportion must be between 0-1! You provided ", grdt_clr_prop)
 
-  if (grdt_start_prop + grdt_decrease_prop > 1)
-    stop("The start and the decrease section must be less or equal to 1: ",
-      grdt_start_prop, " + ", grdt_decrease_prop, " = ", grdt_start_prop + grdt_decrease_prop)
-
-  if (getGridVal(width, default.units) < getGridVal(grdt_line_width, default.units)*2)
-    stop("Your width of your arrow exceeds the width of the line, seems to be ")
+  if (getGridVal(width, default.units) < getGridVal(grdt_line_width, default.units))
+    stop("Your gradient width exceeds the width of the arrow")
 
   # Get the pure arrow
-  pg <- bezierArrowSmpl(width = width, default.units = default.units,
-    clr = clr, align_2_axis = align_2_axis,
-    gp = gp, vp = vp, ...)
-
-  if (grdt_start_prop + grdt_decrease_prop == 0){
-    warning("You called the gradient function but did not",
-            " specify any gradient, why not just use",
-            " the bezierArrowSmpl function")
-    return(pg)
-  }
+  pg <- bezierArrowSmpl(x = x, y = y,
+                        width = width,
+                        default.units = default.units,
+                        clr = clr, align_2_axis = align_2_axis,
+                        gp = gp, vp = vp, ...)
 
   # Now to the gradient
   bp <- attr(pg, "center_points")
@@ -109,17 +120,21 @@ bezierArrowGradient <- function(
   grdt_line_width <- convertX(grdt_line_width, unitTo = internal.units, valueOnly = TRUE)
 
   # Calculate the lengths
-  bp$lengths <-
-    c(0,
-      with(bp,
-         mapply(x1 = x[-length(x)],
-                y1 = y[-length(y)],
-                x2 = x[-1],
-                y2 = y[-1],
-                function(x1, y1, x2, y2) sqrt((x2-x1)^2 + (y2-y1)^2))))
-  bp$cumlen <- cumsum(bp$lengths)
+  lenCalc <- function(bp){
+    bp$lengths <-
+      c(0,
+        with(bp,
+             mapply(x1 = x[-length(x)],
+                    y1 = y[-length(y)],
+                    x2 = x[-1],
+                    y2 = y[-1],
+                    function(x1, y1, x2, y2) sqrt((x2-x1)^2 + (y2-y1)^2))))
+    bp$cumlen <- cumsum(bp$lengths)
+    return(bp)
+  }
+  bp <- lenCalc(bp)
 
-  grdt_length <- sum(bp$lengths)*(grdt_start_prop + grdt_decrease_prop)
+  grdt_length <- sum(bp$lengths)*(grdt_prop)
   end_point <-
     (bp$cumlen - grdt_length) %>%
     abs %>%
@@ -127,72 +142,104 @@ bezierArrowGradient <- function(
 
   # Adjust last point if discrepancy > 10%
   if (abs(bp$cumlen[end_point] - grdt_length) > bp$length[end_point]/10){
-    dx <- bp$x[end_point] - bp$x[end_point - 1]
-    dy <- bp$y[end_point] - bp$y[end_point - 1]
+    if (bp$cumlen[end_point] - grdt_length < 0 &&
+        end_point <= length(bp$x)){
+      dx <- bp$x[end_point + 1] - bp$x[end_point]
+      dy <- bp$y[end_point + 1] - bp$y[end_point]
+      adj_prop <- (grdt_length - bp$cumlen[end_point])/bp$length[end_point + 1]
+    }else{
+      dx <- bp$x[end_point] - bp$x[end_point - 1]
+      dy <- bp$y[end_point] - bp$y[end_point - 1]
+      adj_prop <- (grdt_length - bp$cumlen[end_point])/bp$length[end_point]
+    }
 
     # Desired length vs actual length
-    adj_prop <- (grdt_length - bp$cumlen[end_point])/bp$length[end_point]
     bp$x[end_point] <- bp$x[end_point] +
       dx * adj_prop
     bp$y[end_point] <- bp$y[end_point] +
       dy * adj_prop
     # Adjust the lengths
     bp$lengths[end_point] <- abs(bp$cumlen[end_point] - grdt_length)
-    bp$cumlen[end_point] <- bp$cumlen[end_point] + grdt_length
-  }
+    bp$cumlen[end_point] <- grdt_length
 
-  start_decrease <-
-    (bp$cumlen -
-       sum(bp$lengths)*(grdt_start_prop)) %>%
-    abs %>%
-    which.min
+    # Shorten to the end
+    bp <- lapply(bp, function(x) head(x, n = end_point))
+
+    # If the line was extended with a length beyond the regular
+    # length we should split the last element into sub-elements
+    avg_len <- mean(bp$lengths[-end_point])
+    if (tail(bp$lengths, 1L) > avg_len * 2){
+      no_splits <- round(tail(bp$lengths, 1L)/avg_len)
+      last_2_pt <- lapply(bp, tail, n = 2L)
+      bp <- lapply(bp, head, n = length(bp$x) - 2)
+
+      spltVals <- function(vals, tail_vals, no_splits){
+        c(vals,
+          seq(from = tail_vals[1],
+              to = tail(tail_vals, 1),
+              length.out = no_splits))
+      }
+      bp$x <- spltVals(bp$x, last_2_pt$x, no_splits)
+      bp$y <- spltVals(bp$y, last_2_pt$y, no_splits)
+      bp <- lenCalc(bp)
+    }
+  }else{
+    # Shorten to the end
+    bp <- lapply(bp, function(x) head(x, end_point))
+  }
 
   # Add start margin
+  margin <- grdt_line_width/2
   line_start <-
-    (bp$cumlen -
-       grdt_line_width/2) %>%
+    (bp$cumlen - margin) %>%
     abs %>%
     which.min
 
-  if (bp$cumlen[line_start + 1] - grdt_line_width/2 < 0){
+  if (bp$cumlen[line_start] - margin > 0){
     line_start <- line_start - 1
   }
-
-  clr_start <-
-    (bp$cumlen[line_start:end_point] -
-       bp$cumlen[end_point] * grdt_clr_prop) %>%
-    abs %>%
-    which.min %>%
-    + 1
 
   # Remove start
   if (line_start > 1){
     bp <- lapply(bp, function(x) x[-1*(1:line_start - 1)])
+    if (bp$cumlen[1] != margin){
+      dx <- bp$x[2] - bp$x[1]
+      dy <- bp$y[2] - bp$y[1]
+
+      adj_prop <- (bp$cumlen[2] - margin)/bp$length[2]
+
+      bp$x[1] <- bp$x[1] +
+        dx * adj_prop
+      bp$y[1] <- bp$y[1] +
+        dy * adj_prop
+    }
+
+    bp <- lenCalc(bp)
   }
-  end_point <- end_point - line_start + 1
-  start_decrease <- start_decrease - line_start + 1
 
-  if (abs(bp$cumlen[2] - grdt_line_width/2) > grdt_line_width/10^3){
-    dx <- bp$x[2] - bp$x[1]
-    dy <- bp$y[2] - bp$y[1]
 
-    adj_prop <- (bp$cumlen[2] - grdt_line_width/2)/bp$length[2]
-    bp$x[1] <- bp$x[1] +
-      dx * adj_prop
-    bp$y[1] <- bp$y[1] +
-      dy * adj_prop
-  }
+  ########################
+  # Get the decrease and #
+  # color start points   #
+  ########################
+  start_decrease <-
+    (bp$cumlen - tail(bp$cumlen, 1)*(1-grdt_decrease_prop)) %>%
+    abs %>%
+    which.min
 
-  # Shorten to the end
-  bp <- lapply(bp, function(x) head(x, end_point))
+  clr_start <-
+    (bp$cumlen - tail(bp$cumlen, 1) * (1 - grdt_clr_prop)) %>%
+    abs %>%
+    which.min %>%
+    + 1
+
 
   # The offset decrease should be relative
   offset <- rep(grdt_line_width/2, length(bp$x))
-  if (end_point > start_decrease &&
-        grdt_type == "triangle"){
-    decr_lengths <- tail(bp$lengths, end_point - start_decrease)
-    offset[start_decrease:end_point] <-
-      grdt_line_width/2 *
+  if (grdt_type == "triangle"){
+    decr_lengths <- tail(bp$lengths, length(offset) - start_decrease)
+    offset[start_decrease:length(offset)] <-
+      offset[start_decrease:length(offset)] *
       (1 - c(0, cumsum(decr_lengths))/sum(decr_lengths))
   }
 
@@ -230,26 +277,24 @@ bezierArrowGradient <- function(
                            gp = gpar(fill = grdt_clr, col = grdt_clr, lwd = .01)))
 
     inner_gradient <- gList(inner_gradient, gradient_pg)
-
-    # Remove the already plotted
-    grdnt_lines <-
-      lapply(grdnt_lines,
-             function(side) lapply(side,
-                                   function(x) (x[-1*(1:(clr_start - 2))])))
   }
 
   # Plot the gradient color - these need to be plotted as
   # one color per polygon
   if (length(bp$x) >= clr_start){
-    g_clrs <- colorRampPalette(colors=c(grdt_clr, clr))(length(grdnt_lines$right$x))
-    for (i in 1:(length(grdnt_lines$right$x) - 1)){
+    elmnts <- (clr_start-1):(length(grdnt_lines$right$x)-1)
+    g_clrs <- colorRampPalette(colors=c(grdt_clr, clr))(length(elmnts))
+    for (i in elmnts){
+      col <- g_clrs[i-clr_start+2]
+      x <- with(grdnt_lines, c(left$x[i:(i+1)], right$x[(i+1):i]))
+      y <- with(grdnt_lines, c(left$y[i:(i+1)], right$y[(i+1):i]))
       gradient_pg <-
         with(grdnt_lines,
-             convGenPolyGrob(x = c(left$x[i:(i+1)], right$x[(i+1):i]),
-                             y = c(left$y[i:(i+1)], right$y[(i+1):i]),
+             convGenPolyGrob(x = x,
+                             y = y,
                              d.u = default.units,
                              i.u = internal.units,
-                             gp = gpar(fill = g_clrs[i], col = g_clrs[i], lwd = .01)))
+                             gp = gpar(fill = col, col = col, lwd = .01)))
 
       inner_gradient <- gList(inner_gradient, gradient_pg)
     }
