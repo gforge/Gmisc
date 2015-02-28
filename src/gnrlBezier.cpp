a12118ed8be5afc723e505e8483368f829d32217
Max Gordon <max@gforge.se>
2015-02-28 15:21:58 +0100

    The function can now handle any number of point in any dimension 1 -> inf. It also names the columns of the matrix according to the input matrix.

--- src/gnrlBezier.cpp
+++ src/gnrlBezier.cpp
@@ -9,7 +9,7 @@ NumericMatrix getNumericMatrix(SEXP& x);
 //' and more advanced Bezier lines.
 //'
 //' @param ctrl_points The ctrl_points for the bezier control points. This should
-//'  either be a matrix or a data.frame.
+//'  either be a \code{matrix} or a \code{data.frame}.
 //' @param length_out The length of the return points, i.e. how fine
 //'  detailed the points should be.
 //'
@@ -33,17 +33,18 @@ NumericMatrix getNumericMatrix(SEXP& x);
 //' }
 // [[Rcpp::export]]
 NumericMatrix gnrlBezierPoints(SEXP& ctrl_points, int length_out = 100) {
-  NumericMatrix mx = getNumericMatrix(ctrl_points);
+  NumericMatrix ctrl_mtrx = getNumericMatrix(ctrl_points);
+
   if (length_out < 1)
     throw std::logic_error("The length.out cannot be negative");
-  if (mx.nrow() < 3)
+  if (ctrl_mtrx.nrow() < 3)
       throw std::range_error("The minimum length of the control points is 3");
 
-  NumericMatrix ret(length_out, mx.ncol());
+  NumericMatrix ret(length_out, ctrl_mtrx.ncol());
 
   // We need to set the first element to the starting point
-  for (int col = 0; col < mx.ncol(); col++){
-    ret(0, col) = mx(0, col);
+  for (int col = 0; col < ctrl_mtrx.ncol(); col++){
+    ret(0, col) = ctrl_mtrx(0, col);
   }
   length_out -= 1;
 
@@ -54,19 +55,28 @@ NumericMatrix gnrlBezierPoints(SEXP& ctrl_points, int length_out = 100) {
     else
       t += 1.0/double(length_out);
 
-    for (int col = 0; col < mx.ncol(); col++){
       // Now we need to sum up the elements according to the
       // 1962 Pierre Bézier formula
       // Note that P0 -> Pn the n in the formula is actually n - 1
-      int n = mx.nrow() - 1;
+      int n = ctrl_mtrx.nrow() - 1;
       for (int ii = 0; ii <= n; ii++){
-        ret(i + 1, col) += R::choose(n, ii)*
+        double b_ii_n = R::choose(n, ii)*
           pow(1-t, n - ii) *
-          pow(t, ii) *
-          mx(ii, col);
+          pow(t, ii);
+        for (int col = 0; col < ctrl_mtrx.ncol(); col++){
+          ret(i + 1, col) +=  b_ii_n *
+            ctrl_mtrx(ii, col);
+        }
       }
-    }
   };
+
+
+  List dmn = ctrl_mtrx.attr("dimnames");
+  if (dmn.size() == 2){
+    List dmn_new = Rcpp::List::create(Rcpp::CharacterVector::create(),
+                                      dmn[1]);
+    ret.attr("dimnames") = dmn_new;
+  }
 
   return(ret);
 }
