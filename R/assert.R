e4fd3dc07d95bd0f1a22b76c47eac566ab6e320d
Max Gordon <max@gforge.se>
2017-10-19 07:23:32 +0200

    Removed duplicated assert_just

--- R/assert.R
+++ R/assert.R
@@ -18,26 +18,6 @@ assert_unit <- function(var, allow_missing = TRUE){
   )
 }
 
-#' Assert that its a string or number 0-1
-#'
-#' @param var Varible 2 check
-#' @param allow_missing Allow missing variables
-#'
-#' @return void
-#' @rdname assert
-#' @keywords internal
-#' @importFrom checkmate checkString checkNumeric assert
-assert_just <- function(var, allow_missing = TRUE){
-  if (missing(var) && allow_missing)
-    return()
-
-  assert(
-    checkString(var, pattern = "left|right|top|bottom"),
-    checkNumeric(var, lower = 0, upper = 1),
-    .var.name = dep_var(var))
-}
-
-
 #' Assert that its a string or number
 #'
 #' @param var Varible 2 check
