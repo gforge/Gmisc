bed562e7455e50861f17b50c281979fd06abb12f
Max Gordon <max@gforge.se>
2020-05-05 14:13:00 +0200

    Test locale to be C

--- tests/testthat/test-time2spanTxt.R
+++ tests/testthat/test-time2spanTxt.R
@@ -3,17 +3,17 @@ context('pathJoin')
 
 test_that("time2spanTxt basics", {
   expect_equal(time2spanTxt(as.POSIXct(c("2020-01-02", "2020-03-01", NA)),
-                            day_month_glue_txt = "{mday(time)} {month(time, label = TRUE, locale = 'English')}",
+                            day_month_glue_txt = "{mday(time)} {month(time, label = TRUE, locale = 'C')}",
                             start_stop_glue_txt = "{start}-{stop}"),
                "2 Jan-1 Mar")
 
   expect_equal(time2spanTxt(as.Date(c("2020-01-02", "2020-03-11", NA)),
-                            day_month_glue_txt = "{mday(time)} {month(time, label = TRUE, locale = 'English')}",
+                            day_month_glue_txt = "{mday(time)} {month(time, label = TRUE, locale = 'C')}",
                             start_stop_glue_txt = "{start}-{stop}"),
                "2 Jan-11 Mar")
 
   expect_equal(time2spanTxt(as.Date(c("2020-01-02", "2020-03-11", "2020-02-01")),
-                            day_month_glue_txt = "{mday(time)} {month(time, label = TRUE, locale = 'English')}",
+                            day_month_glue_txt = "{mday(time)} {month(time, label = TRUE, locale = 'C')}",
                             start_stop_glue_txt = "{start}-{stop}"),
                "2 Jan-11 Mar")
 })
