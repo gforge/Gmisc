574db2ec3f252b4f4ab2f25fb21f8f363f938901
Max Gordon <max@gforge.se>
2022-01-02 22:53:43 +0100

    Refactored mergeDesc S3 into separate files

--- R/mergeDesc.R
+++ R/mergeDesc.R
@@ -164,88 +164,10 @@ mergeDesc <- function(..., htmlTable_args = list()) {
     htmlTable_args <- as.list(htmlTable_args)
   }
 
-  return(structure(mx,
+  structure(mx,
     rgroup = rgroup,
     n.rgroup = n.rgroup,
     htmlTable_args = htmlTable_args,
     class = c("descMrg", class(mx))
-  ))
-}
-
-
-#' A wrapper around the \code{\link[htmlTable]{htmlTable}}/\code{\link[Hmisc]{latex}} function
-#'
-#' Calls the \code{\link[htmlTable]{htmlTable}}/\code{\link[Hmisc]{latex}} after extracting the
-#' \code{rgroup}/\code{n.rgroup} arguments.
-#'
-#' @param x The \code{\link{mergeDesc}} object
-#' @param ... Passed onto \code{\link[htmlTable]{htmlTable}}/\code{\link[Hmisc]{latex}}
-#'
-#' @rdname htmlTable_latex
-#' @importFrom Hmisc latex
-#' @keywords internal
-#' @export
-latex.descMrg <- function(x, ...) {
-  dots <- list(...)
-  if (!"rgroup" %in% names(dots)) {
-    return(NextMethod(
-      generic = NULL, object = x,
-      rgroup = attr(x, "rgroup"),
-      n.rgroup = attr(x, "n.rgroup"),
-      ...
-    ))
-  }
-
-  return(NextMethod(
-    generic = NULL, object = x,
-    ...
-  ))
-}
-
-
-#' @rdname htmlTable_latex
-#' @export
-#' @import htmlTable
-#' @import magrittr
-htmlTable.descMrg <- function(x, ...) {
-  dots <- list(...)
-  htmlTable_args <- attr(x, "htmlTable_args")
-  for (n in names(htmlTable_args)) {
-    if (!n %in% names(dots)) {
-      dots[[n]] <- htmlTable_args[[n]]
-    }
-  }
-
-  # Merge calls
-  class(x) <- class(x)[!(class(x) %in% "descMrg")]
-  args <- c(
-    list(x = x),
-    dots
   )
-  if (!"rgroup" %in% names(args)) {
-    args[["rgroup"]] <- attr(x, "rgroup")
-    args[["n.rgroup"]] <- attr(x, "n.rgroup")
-  }
-
-  fastDoCall(htmlTable, args)
-}
-
-
-#' @rdname htmlTable_latex
-#' @export
-#' @import htmlTable
-print.descMrg <- function(x, ...) {
-  htmlTable(x, ...) %>%
-    print()
-}
-
-
-#' @rdname htmlTable_latex
-#' @import htmlTable
-#' @export
-#' @importFrom knitr asis_output
-#' @importFrom knitr knit_print
-knit_print.descMrg <- function(x, ...) {
-  htmlTable(x, ...) %>%
-    knit_print()
 }
